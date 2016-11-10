a9c0d02047168c54cb22afe9440396196441c04d
Armienn <nigathr@gmail.com>
2016-11-10 11:31:22 +0100

    Added generation of language sounds

--- language/Language.go
+++ language/Language.go
@@ -51,9 +51,32 @@ func RandomLanguage() *Language {
 	info := SoundInformation{}
 	pattern := SyllablePattern{}
 
-	soundCount := rand.Intn(20)
-	for i := 0; i < soundCount; i++ {
-		info.Sound = RandomSound()
+	basePoints := randomPoints()
+	baseVoices := randomVoices()
+
+	sounds := make([]Sound, 0)
+
+	sounds = append(sounds, randomSoundSet(basePoints, baseVoices, Closed)...)
+	sounds = append(sounds, randomSoundSet(basePoints, baseVoices, Stop)...)
+	sounds = append(sounds, randomSoundSet(basePoints, baseVoices, Fricative)...)
+	if rand.Intn(10) < 2 {
+		sounds = append(sounds, randomSoundSet(basePoints, baseVoices, Flap)...)
+	}
+	if rand.Intn(10) < 2 {
+		sounds = append(sounds, randomSoundSet(basePoints, baseVoices, Trill)...)
+	}
+	if rand.Intn(10) < 2 {
+		sounds = append(sounds, randomSoundSet(basePoints, baseVoices, Approximant)...)
+	}
+	sounds = append(sounds, randomVocalSet(baseVoices)...)
+
+	for i := 0; i < 10; i++ {
+		sounds = mutateSoundSet(sounds)
+	}
+
+	for i := range sounds {
+		sounds[i].Standardise()
+		info.Sound = sounds[i]
 		info.Representation = "s" + fmt.Sprint(i)
 		language.Sounds = append(language.Sounds, info)
 	}
@@ -84,8 +107,152 @@ func RandomLanguage() *Language {
 				Open,
 			}})
 	language.Patterns = append(language.Patterns, pattern)
+	pattern = SyllablePattern{}
+	pattern.CodaPatterns = make([]SoundPattern, 0)
+	pattern.CodaPatterns = append(pattern.CodaPatterns,
+		SoundPattern{
+			Manners: []ArticulationManner{
+				Closed,
+				Stop,
+				Flap,
+				Trill,
+				Fricative,
+				Approximant,
+			}})
+	language.Patterns = append(language.Patterns, pattern)
+	pattern = SyllablePattern{}
+	pattern.OnsetPatterns = make([]SoundPattern, 0)
+	pattern.OnsetPatterns = append(pattern.OnsetPatterns,
+		SoundPattern{
+			Manners: []ArticulationManner{
+				Closed,
+				Stop,
+				Flap,
+				Trill,
+				Fricative,
+				Approximant,
+			}})
+	language.Patterns = append(language.Patterns, pattern)
 
 	return language
+}
+
+func randomPoints() []ArticulationPoint {
+	pointCount := rand.Intn(4) + 1
+	points := make([]ArticulationPoint, pointCount)
+	for i := 0; i < pointCount; i++ {
+		newPoint := ArticulationPoint(rand.Intn(int(ArticulationPointCount)))
+		valid := true
+		for j := 0; j < i; j++ {
+			difference := newPoint - points[j]
+			if -2 < difference && difference < 2 {
+				valid = false
+				break
+			}
+		}
+		if valid {
+			points[i] = newPoint
+		} else {
+			i--
+		}
+	}
+	return points
+}
+
+func randomVoices() []Voice {
+	voices := make([]Voice, 0)
+	if rand.Intn(10) < 2 {
+		voices = append(voices, Breathy)
+	}
+	if rand.Intn(10) < 2 {
+		voices = append(voices, Creaky)
+	}
+	if rand.Intn(2) == 0 {
+		voices = append(voices, Voiceless)
+		if rand.Intn(5)-len(voices) > 0 {
+			voices = append(voices, Modal)
+		}
+	} else {
+		voices = append(voices, Modal)
+		if rand.Intn(5)-len(voices) > 0 {
+			voices = append(voices, Voiceless)
+		}
+	}
+	return voices
+}
+
+func chooseRandomVoices(voices []Voice) []Voice {
+	chosenVoices := make([]Voice, 0)
+	for _, voice := range voices {
+		if rand.Intn(2) == 0 {
+			chosenVoices = append(chosenVoices, voice)
+		}
+	}
+	if len(chosenVoices) == 0 {
+		chosenVoices = append(chosenVoices, voices[rand.Intn(len(voices))])
+	}
+	return chosenVoices
+}
+
+func randomSoundSet(points []ArticulationPoint, voices []Voice, manner ArticulationManner) []Sound {
+	chosenVoices := chooseRandomVoices(voices)
+	sounds := make([]Sound, len(chosenVoices)*len(points))
+	shape := TongueShape(rand.Intn(int(TongueShapeCount)))
+	nasal := false
+	if manner == Closed {
+		nasal = true
+	}
+	rounded := false
+	for i := 0; i < len(chosenVoices); i++ {
+		for j := 0; j < len(points); j++ {
+			sound := Sound{points[j], manner, shape, rounded, nasal, chosenVoices[i]}
+			sounds[i*len(points)+j] = sound
+		}
+	}
+	return sounds
+}
+
+func randomVocalSet(voices []Voice) []Sound {
+	chosenVoices := make([]Voice, 1)
+	chosenVoices[0] = Modal
+	for _, voice := range voices {
+		if rand.Intn(2) == 0 && (voice == Breathy || voice == Creaky) {
+			chosenVoices = append(chosenVoices, voice)
+		}
+	}
+
+	sounds := make([]Sound, 5*len(chosenVoices))
+	for i, voice := range chosenVoices {
+		sounds[i*5] = Sound{DorsalPalatal, Close, Central, rand.Intn(4) == 0, false, voice}
+		sounds[i*5+1] = Sound{DorsalPalVel, Mid, Central, rand.Intn(4) == 0, false, voice}
+		sounds[i*5+2] = Sound{DorsalVelar, Open, Central, rand.Intn(4) == 0, false, voice}
+		sounds[i*5+3] = Sound{DorsalVelUlu, Mid, Central, rand.Intn(4) == 0, false, voice}
+		sounds[i*5+4] = Sound{DorsalUvular, Close, Central, rand.Intn(4) == 0, false, voice}
+	}
+	return sounds
+}
+
+func mutateSoundSet(sounds []Sound) []Sound {
+	index := rand.Intn(len(sounds))
+	if rand.Intn(2) == 0 {
+		return append(sounds[:index], sounds[index+1:]...)
+	}
+	sound := sounds[index]
+	if rand.Intn(2) == 0 {
+		change := ArticulationManner(rand.Intn(2)*2 - 1)
+		sound.Manner = sound.Manner + change
+		if sound.Manner == ArticulationMannerCount {
+			sound.Manner = sound.Manner - change*2
+		}
+	}
+	if rand.Intn(2) == 0 {
+		change := ArticulationPoint(rand.Intn(2)*2 - 1)
+		sound.Point = sound.Point + change
+		if sound.Point == ArticulationPointCount {
+			sound.Point = sound.Point - change*2
+		}
+	}
+	return append(sounds, sound)
 }
 
 func GetDansk() *Language {
--- start.go
+++ start.go
@@ -10,9 +10,12 @@ import (
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
+	//rand.Seed(1)
 	lang := language.RandomLanguage()
+	dansk := language.GetDansk()
 	for i := 0; i < 10; i++ {
 		word := lang.RandomWord(0)
 		fmt.Println(lang.GetRepresentation(word))
+		fmt.Println(dansk.GetRepresentation(word))
 	}
 }
